96a4ed2653ef2ed12a8deab2424792790abdf257
Nicholas Hunt-Walker <nhuntwalker@gmail.com>
2017-01-14 18:13:41 -0800

    making changes to the structure of the back end

--- rational_whimsy/rational_whimsy/views.py
+++ rational_whimsy/rational_whimsy/views.py
@@ -6,7 +6,7 @@ from blog.models import Post
 def home_view(request):
     """Simple view for the home page listing blog posts."""
     featured_post = Post.published.filter(featured=True).first()
-    posts = Post.published.all().exclude(pk=featured_post.pk)
+    posts = Post.published.all().exclude(pk=featured_post.pk)[:5]
     return render(request, "rational_whimsy/home.html", {
         "posts": posts,
         "featured_post": featured_post
