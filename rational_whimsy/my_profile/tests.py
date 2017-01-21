1512ea51172c51fc70ee0937527e262f02219f3d
Nicholas Hunt-Walker <nhuntwalker@gmail.com>
2017-01-20 17:22:39 -0800

    Refactored testts to be consistent with login

--- rational_whimsy/my_profile/tests.py
+++ rational_whimsy/my_profile/tests.py
@@ -7,6 +7,8 @@ from faker import Faker
 from django.forms import ModelForm
 from django.urls import reverse_lazy
 from bs4 import BeautifulSoup
+from django.contrib.sessions.middleware import SessionMiddleware
+
 
 fake = Faker()
 
@@ -67,6 +69,13 @@ class ProfileViewTests(TestCase):
         self.request = RequestFactory()
         self.get_req = RequestFactory().get("/foo_path")
 
+    def add_session_middleware(self, request):
+        """Need to add session middleware for authentication."""
+        mdl = SessionMiddleware()
+        mdl.process_request(request)
+        request.session.save()
+        request.user = self.user
+
     def test_profile_detail_view_has_details(self):
         """Information from the profile should be in the response."""
         from my_profile.views import profile_detail
@@ -85,88 +94,6 @@ class ProfileViewTests(TestCase):
         """The detail view should use the my_profile/about.html template."""
         response = self.client.get(reverse_lazy("profile"))
         self.assertTemplateUsed(response, "my_profile/about.html")
-
-    def test_profile_edit_get_is_form(self):
-        """A simple get request returns a form in HTML."""
-        from my_profile.views import profile_edit
-        response = profile_edit(self.get_req)
-        html = BeautifulSoup(response.content, "html5lib")
-        self.assertTrue(len(html.find_all("form")) == 1)
-
-    def test_profile_edit_form_has_fields(self):
-        """A GET request returns all the form fields."""
-        from my_profile.views import profile_edit
-        response = profile_edit(self.get_req)
-        html = BeautifulSoup(response.content, "html5lib")
-        desired_fields = ["linkedin", "github", "twitter",
-                          "facebook", "instagram"]
-        for field in desired_fields:
-            self.assertTrue(html.find("input", {"name": field}) is not None)
-        self.assertTrue(html.find("textarea",
-                                  {"name": "description"}) is not None)
-        self.assertTrue(html.find("select", {"name": "photo"}) is not None)
-
-    def test_profile_response_has_form_in_context(self):
-        """A get request returns a form object in the context."""
-        response = self.client.get(reverse_lazy("profile_edit"))
-        self.assertIsInstance(response.context["form"], ModelForm)
-
-    def test_profile_edit_form_with_post_redirects_on_success(self):
-        """A post request redirects on success."""
-        from my_profile.views import profile_edit
-        response = profile_edit(self.request.post("/foo_path", {
-            "photo": "",
-            "linkedin": "",
-            "github": "",
-            "twitter": "",
-            "facebook": "",
-            "instagram": "",
-            "description": "pancakes"
-        }))
-        self.assertTrue(response.status_code == 302)
-
-    def test_profile_edit_route_with_post_redirects_to_profile(self):
-        """A post request redirects to the profile page."""
-        response = self.client.post(reverse_lazy("profile_edit"), {
-            "photo": "",
-            "linkedin": "",
-            "github": "",
-            "twitter": "",
-            "facebook": "",
-            "instagram": "",
-            "description": "pancakes"
-        }, follow=True)
-        chain = response.redirect_chain
-        self.assertTrue(chain[0][0] == reverse_lazy("profile"))
-
-    def test_profile_edit_view_with_post_changes_model_attrs(self):
-        """A post request to edit view changes the model attributes."""
-        from my_profile.views import profile_edit
-        profile_edit(self.request.post("/foo_path", {
-            "photo": "",
-            "linkedin": "",
-            "github": "",
-            "twitter": "",
-            "facebook": "",
-            "instagram": "",
-            "description": "pancakes"
-        }))
-        profile = NMHWProfile.objects.get(user__username="nhuntwalker")
-        self.assertTrue(profile.description == "pancakes")
-
-    def test_profile_edit_route_with_post_changes_model_attrs(self):
-        """A post request to the edit route changes the model attributes."""
-        self.client.post(reverse_lazy("profile_edit"), {
-            "photo": "",
-            "linkedin": "",
-            "github": "",
-            "twitter": "",
-            "facebook": "",
-            "instagram": "",
-            "description": "pancakes"
-        })
-        profile = NMHWProfile.objects.get(user__username="nhuntwalker")
-        self.assertTrue(profile.description == "pancakes")
 
     def test_profile_login_route_get_shows_form(self):
         """A get request to the login route shows a login form."""
@@ -198,4 +125,108 @@ class ProfileViewTests(TestCase):
             "username": self.user.username,
             "password": "potatoes"
         })
-        self.assertTrue(response.url == reverse_lazy("home"))
+        self.assertTrue(response.url == reverse_lazy("home_page"))
+
+    def test_profile_login_route_logs_in_users(self):
+        """A logged in user is authenticated."""
+        self.client.post(reverse_lazy("login"), {
+            "username": self.user.username,
+            "password": "potatoes"
+        })
+        self.assertTrue(self.user.is_authenticated)
+
+    def test_profile_edit_get_is_form(self):
+        """A simple get request returns a form in HTML."""
+        from my_profile.views import profile_edit
+        self.get_req.user = self.user
+        response = profile_edit(self.get_req)
+        html = BeautifulSoup(response.content, "html5lib")
+        self.assertTrue(len(html.find_all("form")) == 1)
+
+    def test_profile_edit_form_has_fields(self):
+        """A GET request returns all the form fields."""
+        from my_profile.views import profile_edit
+        self.get_req.user = self.user
+        response = profile_edit(self.get_req)
+        html = BeautifulSoup(response.content, "html5lib")
+        desired_fields = ["linkedin", "github", "twitter",
+                          "facebook", "instagram"]
+        for field in desired_fields:
+            self.assertTrue(html.find("input", {"name": field}) is not None)
+        self.assertTrue(html.find("textarea",
+                                  {"name": "description"}) is not None)
+        self.assertTrue(html.find("select", {"name": "photo"}) is not None)
+
+    def test_profile_edit_response_has_form_in_context(self):
+        """A get request returns a form object in the context."""
+        self.client.force_login(self.user)
+        response = self.client.get(reverse_lazy("profile_edit"))
+        self.assertIsInstance(response.context["form"], ModelForm)
+
+    def test_profile_edit_form_with_post_redirects_on_success(self):
+        """A post request redirects on success."""
+        from my_profile.views import profile_edit
+        post_req = self.request.post("/foo_path", {
+            "photo": "",
+            "linkedin": "",
+            "github": "",
+            "twitter": "",
+            "facebook": "",
+            "instagram": "",
+            "description": "pancakes"
+        })
+        post_req.user = self.user
+        response = profile_edit(post_req)
+        self.assertTrue(response.status_code == 302)
+
+    def test_profile_edit_route_with_post_redirects_to_profile(self):
+        """A post request redirects to the profile page."""
+        self.client.force_login(self.user)
+        response = self.client.post(reverse_lazy("profile_edit"), {
+            "photo": "",
+            "linkedin": "",
+            "github": "",
+            "twitter": "",
+            "facebook": "",
+            "instagram": "",
+            "description": "pancakes"
+        }, follow=True)
+        chain = response.redirect_chain
+        self.assertTrue(chain[0][0] == reverse_lazy("profile"))
+
+    def test_profile_edit_view_with_post_changes_model_attrs(self):
+        """A post request to edit view changes the model attributes."""
+        from my_profile.views import profile_edit
+        post_req = self.request.post("/foo_path", {
+            "photo": "",
+            "linkedin": "",
+            "github": "",
+            "twitter": "",
+            "facebook": "",
+            "instagram": "",
+            "description": "pancakes"
+        })
+        post_req.user = self.user
+        profile_edit(post_req)
+        profile = NMHWProfile.objects.get(user__username="nhuntwalker")
+        self.assertTrue(profile.description == "pancakes")
+
+    def test_profile_edit_route_with_post_changes_model_attrs(self):
+        """A post request to the edit route changes the model attributes."""
+        self.client.force_login(self.user)
+        self.client.post(reverse_lazy("profile_edit"), {
+            "photo": "",
+            "linkedin": "",
+            "github": "",
+            "twitter": "",
+            "facebook": "",
+            "instagram": "",
+            "description": "pancakes"
+        })
+        profile = NMHWProfile.objects.get(user__username="nhuntwalker")
+        self.assertTrue(profile.description == "pancakes")
+
+    def test_unauthenticated_user_profile_edit_route_redirects_login(self):
+        """."""
+        response = self.client.get(reverse_lazy("profile_edit"), follow=True)
+        self.assertTrue(response.request["PATH_INFO"] == reverse_lazy("login"))
--- rational_whimsy/my_profile/views.py
+++ rational_whimsy/my_profile/views.py
@@ -1,5 +1,7 @@
 """Views for the Profile app."""
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
 
 from my_profile.models import NMHWProfile
 from my_profile.forms import EditProfileForm
@@ -12,6 +14,7 @@ def profile_detail(request):
     return render(request, "my_profile/about.html", {"profile": profile})
 
 
+@login_required(login_url=reverse_lazy("login"))
 def profile_edit(request):
     """Edit a single user profile."""
     profile = NMHWProfile.objects.get(user__username="nhuntwalker")
